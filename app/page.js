23869d517ddbb46e0e89df90069256cc791e5840
sol <nathf1005@naver.com>
2024-10-05 02:58:44 +0900

    여러 페이지 만들기 (라우팅)

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   let name = 'park'
   let link = 'http://google.com'
   return (
     <div>
-      <h4 style={{ color: 'red', fontSize: '30px' }}>애플후레시</h4>
+      <h4 className="title">애플후레시</h4>
       <p className="title-sub">by dev {name}</p>
       <a href={link}>링크</a>
     </div>

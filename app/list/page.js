b6283b7856be1be352a0c8bf171e51146e70d1f5
sol <nathf1005@naver.com>
2024-10-05 11:02:14 +0900

    html을 반복문으로 줄이고 싶으면 map

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,22 @@
 
 export default function List() {
-    return (
-      <div>
-        <h4 className="title">상품목록</h4>
-        <div className="food">
-            <h4>상품1 $40</h4>
-        </div>
-        <div className="food">
-            <h4>상품2 $40</h4>
-        </div>
-      </div>
-    );
-  }
-  
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+
+  let a = [20, 30, 40]
+
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      {
+        상품.map((v, i) => {
+          console.log(v);
+          return (
+            <div className="food">
+              <h4>{v} $40</h4>
+            </div>
+          )
+        })
+      }
+    </div >
+  );
+}

5b0293c9aaab26cabbe6f442c8ee1d25d74c3f5c
sol <nathf1005@naver.com>
2024-10-05 14:21:49 +0900

    좋아요 버튼 만들기 (useState, onClick)

--- app/list/page.js
+++ app/list/page.js
@@ -1,23 +1,25 @@
 // import Image from "next/image";
 // import tomato from "/public/food0.png";
 
+'use client'
+
+import { useState } from "react";
 
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-
-  let a = [20, 30, 40]
-
+  const [수량, 수량변경] = useState(0)
   return (
     <div>
       <h4 className="title">상품목록</h4>
       {
         상품.map((v, i) => {
-          console.log(v);
           return (
             <div className="food" key={i}>
               {/* <Image src={tomato} className="food-img"/> */}
-              <img src={`/food${i}.png`} className="food-img"/>
+              <img src={`/food${i}.png`} className="food-img" />
               <h4>{v} $40</h4>
+              <span> {수량} </span>
+              <button onClick={() => { 수량변경(수량+1) }}>+</button>
             </div>
           )
         })

a0e269d8adf1b2cf8a3bddf2b76b5ec82842d5a7
sol <nathf1005@naver.com>
2024-10-05 12:14:12 +0900

    client/server component, import 문법

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,32 @@
+// 'use client'
+
+// server Component
+// html에 자바스크립트 기능넣기 불가능
+// useState, useEffect 등 사용불가
+import age from "./data"
+
 export default function Cart() {
     return (
-      <div>
-        <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
+        <div>
+            <h4 className="title">Cart</h4>
+            <div className="cart-item">
+            <p>상품명 {age}</p>
+            <p>$40</p>
+            <p>1개</p>
         </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
+            <CartItem />
+            <CartItem />
+            <CartItem />
         </div>
-      </div>
     )
-  } 
+}
+
+function CartItem(props) {
+    return (
+        <div className="cart-item">
+            <p>상품명</p>
+            <p>$40</p>
+            <p>1개</p>
+        </div>
+    )
+}

d9ccc767b1f2100b9c5c0617a42c11a80807dba2
sol <nathf1005@naver.com>
2024-10-05 12:37:58 +0900

    Component에 데이터 전해주려면 props

--- app/cart/page.js
+++ app/cart/page.js
@@ -6,25 +6,27 @@
 import age from "./data"
 
 export default function Cart() {
+    let 장바구니 = ['Tomatoes', 'Pasta']
     return (
         <div>
             <h4 className="title">Cart</h4>
-            <div className="cart-item">
-            <p>상품명 {age}</p>
-            <p>$40</p>
-            <p>1개</p>
-        </div>
-            <CartItem />
-            <CartItem />
-            <CartItem />
+            <CartItem item={장바구니[0]} />
+            <CartItem item={장바구니[1]} />
+            <Banner content="롯데카드"/>
+            <Banner content="현대카드"/>
         </div>
     )
 }
 
+function Banner(props) {
+    return <h5>{props.content} 결제 행사중</h5>
+}
+
 function CartItem(props) {
     return (
+
         <div className="cart-item">
-            <p>상품명</p>
+            <p>{props.item}</p>
             <p>$40</p>
             <p>1개</p>
         </div>
